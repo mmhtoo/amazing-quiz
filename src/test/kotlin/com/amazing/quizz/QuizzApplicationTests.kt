0b51d3ccfaf34c441a9c878a31cd5df6eae90a21
myominhtoo <mmhtoo.dev@gmail.com>
2023-12-20 21:12:36 +0630

    project was inited

---
+++ src/test/kotlin/com/amazing/quizz/QuizzApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.amazing.quizz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuizzApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
